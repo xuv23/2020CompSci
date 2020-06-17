63d5b55d49ca83ac250880acedd447481c39537f
xuv23 <66146488+xuv23@users.noreply.github.com>
2020-06-17 14:19:42 -0500

    Update MealChallenge.java

--- src/com/company/MealChallenge.java
+++ src/com/company/MealChallenge.java
@@ -4,13 +4,18 @@
 //      print a line that says "meal cost:"
 //      create a double variable and initialize it so that it equals the next imputed value
 //      print line that says "tip percent:"
-//      create a double variable called "tipPercent" and initialize it so that it equals the next imputed value
-//      create a double variable called "tip" and initialize it so that it equals to "tipPercent/100*mealCost"- this basically tells you the value of the tip
+//      create a integer variable called "tipPercent" and initialize it so that it equals the next imputed value
 //      print line that says "tax percent:"
-//      create a double variable called "taxPercent" and initialize it so that it equals the next imputed value
-//      create a double variable called "tax" and initialize it so that it equals to "taxPercent/100*mealCost"- this basically tells you the value of the tax
-//      create an integer called "totalCost" and initialize it to "(int)Math.round(mealCost+tip+tax)"- which basically is the tip, tax, and meal cost added together, rounded to a whole number, and converted into an integer(gives you total cost of meal including tax and tip)
-//      print a line that says "totalCost"
+//      create a integer variable called "taxPercent" and initialize it so that it equals the next imputed value
+//      call function "solve"
+//      end main function
+//Create a function called "solve" and insert variables "mealCost", "tipPercent", and "taxPercent" into it 
+//      begin function solve
+//              create a double variable called "tip" and initialize it so that it equals to "tipPercent/100*mealCost"- this basically tells you the value of the tip
+//              create a double variable called "tax" and initialize it so that it equals to "taxPercent/100*mealCost"- this basically tells you the value of the tax
+//              create an integer called "totalCost" and initialize it to "(int)Math.round(mealCost+tip+tax)"- which basically is the tip, tax, and meal cost added together, rounded and converted into an integer(gives you total cost of meal including tax and tip)
+//              print a line that says "totalCost"
+//      end function
 package com.company;
 import java.util.*;
 public class MealChallenge {
@@ -20,15 +25,19 @@ public class MealChallenge {
         Double mealCost= input.nextDouble();
 
         System.out.println("tip percent:");
-        Double tipPercent= input.nextDouble();
-        double tip= tipPercent*mealCost/100;
+        int tipPercent= input.nextInt();
 
         System.out.println("tax percent:");
-        Double taxPercent= input.nextDouble();
-        double tax= (taxPercent/100)*mealCost;
+        int taxPercent= input.nextInt();
 
+        solve(mealCost, tipPercent, taxPercent);
+
+    }
+    public static void solve(double mealCost, int tipPercent, int taxPercent){
+        double tip= tipPercent*mealCost/100;
+        double tax= taxPercent*mealCost/100;
         int totalCost= (int)Math.round(mealCost+tip+tax);
-        System.out.print(totalCost);
+        System.out.print("Total Cost: "+totalCost);
 
     }
 }
