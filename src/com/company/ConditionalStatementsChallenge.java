7111c5863e230851c17dc2103e032d99854ef8a5
xuv23 <66146488+xuv23@users.noreply.github.com>
2020-06-18 21:47:55 -0500

    Update ConditionalStatementsChallenge.java

--- src/com/company/ConditionalStatementsChallenge.java
+++ src/com/company/ConditionalStatementsChallenge.java
@@ -1,6 +1,14 @@
 // Valerie Xu
 // Create main function
 //      import scanner
+//      Print a line that says "Do you want to play the game(Y or N): "
+//      create a string called "play" and initialize it so that it is equaled to the next input value
+//      While string "play" equals "Y"
+//              then call function "playGame"
+//              Print a line that says "Do you want to play the game(Y or N): "
+//              initialize string "play" again to the next input value
+//      end while loop
+//Create a function called "playGame"
 //      print a line that says "Please type number between 1 and 137: "
 //      create an integer called "n" and initialize it so that it is equaled to the next input value
 //      if n is less than 1 or greater than 137 (n<1 || n>137)
@@ -10,30 +18,41 @@
 //      else (basically if n is between 1 and 137 and is even)
 //              if n is greater and equal to 2 but less than and equal to 5
 //                      then print a line that says "Not Weird"
-//              if n is greater and equal to 6 but less than and equal to 20
+//              else if n is greater and equal to 6 but less than and equal to 20
 //                      then print a line that says "Weird"
-//              if n is greater than 20
+//              else (basically if n is greater than 20)
 //                      then print a line that says "Not Weird"
 
 package com.company;
 import java.util.*;
 public class ConditionalStatementsChallenge {
-    public static void main(String[] args){
-        Scanner input= new Scanner(System.in);
-        System.out.print("Please type number between 1 and 137: ");
-        int n= input.nextInt();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Do you want to play the game(Y or N): ");
+        String play = input.next();
+        input.nextLine();
+        while (play.toUpperCase().equals("Y")) {
+            playGame(input);
+            System.out.print("Do you want to play the game(Y or N): ");
+            play = input.next();
+        }
 
-        if (n<1 || n>137){
-            System.out.print ("Please run again and type in number between 1 and 137");
-        }else if (n%2!=0){
-            System.out.print("Weird");
-        }else{
-            if(n>=2 && n<=5){
-                System.out.print("Not Weird");
-            }else if(n>=6 && n<=20){
-                System.out.print("Weird");
-            }else{
-                System.out.print("Not Weird");
+        System.out.println("Have a great day! bye!");
+    }
+    public static void playGame(Scanner input){
+        System.out.println("Please type number between 1 and 137: ");
+        int n = input.nextInt();
+        if (n < 1 || n > 137) {
+            System.out.println("Please run again and type in number between 1 and 137");
+        } else if (n % 2 != 0) {
+            System.out.println("Weird");
+        } else {
+            if (n >= 2 && n <= 5) {
+                System.out.println("Not Weird");
+            } else if (n >= 6 && n <= 20) {
+                System.out.println("Weird");
+            } else {
+                System.out.println("Not Weird");
             }
         }
     }
